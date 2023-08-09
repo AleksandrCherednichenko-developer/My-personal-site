ca0f5f57562507334fe28c80ce4dd94ada6786f6
AleksandrCherednichenko <acherednichenko174@gmail.com>
2023-08-09 23:00:07 +0700

    feat: -Change transition in resume page;

--- src/views/ResumePage/index.vue
+++ src/views/ResumePage/index.vue
@@ -1,61 +1,49 @@
 <template>
     <section class="section resume__page">
-        <transition
-            name="fade-slide-left"
-            mode="out-in"
-            appear
-        >
-            <div class="page-inner content-container">
-                <UITitlePage title="navbar.resume" />
+        <div class="page-inner content-container">
+            <UITitlePage title="navbar.resume" />
 
-                <div class="resume__page-experience__block">
-                    <transition name="fade-enter-transition" mode="out-in" appear>
+            <transition name="fade-slide-top" mode="out-in" appear>
+                <div class="page-inner">
+                    <div class="resume__page-experience__block">
                         <h5 class="title">
                             {{ $t('experience') }}
                         </h5>
-                    </transition>
-
-                    <template v-for="index in 3" :key="index">
-                        <InfoBlock
-                            :period="$t(`work${index}.period`)"
-                            :name="$t(`work${index}.name`)"
-                            :position="$t(`work${index}.position`)"
-                            :skills="$t(`work${index}.skills`)"
-                            :location="$t(`work${index}.location`)"
-                            :description="$t(`work${index}.description`)"
-                        />
-                    </template>
-                </div>
-                <div class="resume__page-education__block">
-                    <transition name="fade-enter-transition" mode="out-in" appear>
+                        <template v-for="index in 3" :key="index">
+                            <InfoBlock
+                                :period="$t(`work${index}.period`)"
+                                :name="$t(`work${index}.name`)"
+                                :position="$t(`work${index}.position`)"
+                                :skills="$t(`work${index}.skills`)"
+                                :location="$t(`work${index}.location`)"
+                                :description="$t(`work${index}.description`)"
+                            />
+                        </template>
+                    </div>
+                    <div class="resume__page-education__block">
                         <h5 class="title">
                             {{ $t('education') }}
                         </h5>
-                    </transition>
-
-                    <template v-for="index in 2" :key="index">
-                        <InfoBlock
-                            :period="$t(`education${index}.period`)"
-                            :name="$t(`education${index}.name`)"
-                            :position="$t(`education${index}.position`)"
-                            :location="$t(`education${index}.location`)"
-                            :description="$t(`education${index}.description`)"
-                            no-info-block
-                        />
-                    </template>
-                </div>
-
-                <div class="resume__page-skills__block">
-                    <transition name="fade-enter-transition" mode="out-in" appear>
+                        <template v-for="index in 2" :key="index">
+                            <InfoBlock
+                                :period="$t(`education${index}.period`)"
+                                :name="$t(`education${index}.name`)"
+                                :position="$t(`education${index}.position`)"
+                                :location="$t(`education${index}.location`)"
+                                :description="$t(`education${index}.description`)"
+                                no-info-block
+                            />
+                        </template>
+                    </div>
+                    <div class="resume__page-skills__block">
                         <h5 class="title">
                             {{ $t('skills') }}
                         </h5>
-                    </transition>
-
-                    <SkillsBlock />
+                        <SkillsBlock />
+                    </div>
                 </div>
-            </div>
-        </transition>
+            </transition>
+        </div>
     </section>
 </template>
 
